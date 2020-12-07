5efd2ac90d4b0f2e0976988cbb8fd2d6aa0e9cff
Saeed Khan <saeedkhan@Saeeds-MacBook-Pro.local>
2020-12-07 13:24:01 -0700

    1-5 complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,61 @@
-// Your code goes here
+// 1. click turns logo green
+const logo = document.querySelector('h1.logo-heading');
+
+logo.addEventListener("click", () => {
+    logo.style.color = 'green';
+})
+
+// 2. Hover turns nav bar green
+const anchorTags = document.querySelector('.nav');
+
+console.log(anchorTags);
+anchorTags.addEventListener("mouseover", () => {
+    anchorTags.style.color = "green";
+})
+// 3. Enlarge photo frame
+const doubleClick = document.querySelector('div.img-content');
+
+doubleClick.addEventListener("dblclick", () => {
+    doubleClick.style.transform = 'scale(2.0)'
+});
+
+doubleClick.addEventListener('mouseout', () => {
+    doubleClick.style.transform = 'scale(1.0)'
+})
+// 4. Change button color
+const button = document.querySelector('.btn');
+
+function random(number) {
+    return Math.floor(Math.random() * number);
+}
+
+function randomColor() {
+    return `rgb(${random(255)}, ${random(255)}, ${random(255)})`;
+}
+
+button.onclick = function() {
+    button.style.backgroundColor = randomColor();
+    button.style.color = randomColor();
+}
+
+button.addEventListener('onauxclick', (e) => {
+    e.preventDefault();
+    button.style.background = randomColor();
+});
+
+button.oncontextmenu = (e) => {
+    e.preventDefault();
+}
+
+// 5. Copy Menu 
+
+const copy = document.querySelector('div.text-content.p');
+
+copy.addEventListener('copy', (e) => {
+    const selection = document.getSelection();
+    e.clipboardData.setData('text/plain', selection.toString().toUpperCase());
+    e.preventDefault();
+
+})
+
+
